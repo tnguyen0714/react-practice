499f75f49d08c1f7aaeace578d81d0284343fc6f
Tom Nguyen <tomnguyen2014@gmail.com>
2024-03-15 21:10:58 -0400

    Added social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div>
+        <a
+          href="https://www.facebook.com/smiski.japan/"
+          target="_blank"
+          class="fl-icon fl-icon-color-branded fl-icon-facebook fl-icon-facebook-circle"
+        ></a>
+        <a
+          href="https://twitter.com/smiski_usa_"
+          target="_blank"
+          class="fl-icon fl-icon-color-branded fl-icon-twitter fl-icon-twitter-circle"
+        ></a>
+        <a
+          href="https://www.youtube.com/channel/UCkKGZJ-oZ6PhxFVI_Ppm26g"
+          target="_blank"
+          class="fl-icon fl-icon-color-branded fl-icon-youtube fl-icon-youtube-circle"
+        ></a>
+        <a
+          href="https://www.instagram.com/smiski_usa/"
+          target="_blank"
+          class="fl-icon fl-icon-color-branded fl-icon-instagram fl-icon-instagram-circle"
+        ></a>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
