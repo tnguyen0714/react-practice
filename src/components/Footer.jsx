69efe0d9f9a348a3919035feba45f1e1c71be326
Tom Nguyen <tomnguyen2014@gmail.com>
2024-03-18 21:48:17 -0400

    Attempted to turn social media links to their respective icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,37 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <div>
-        <a
-          href="https://www.facebook.com/smiski.japan/"
-          // target="_blank"
-          // class="fl-icon fl-icon-color-branded fl-icon-facebook fl-icon-facebook-circle"
-        >Facebook</a>
-        <a
-          href="https://twitter.com/smiski_usa_"
-          // target="_blank"
-          // class="fl-icon fl-icon-color-branded fl-icon-twitter fl-icon-twitter-circle"
-        >Twitter</a>
-        <a
-          href="https://www.youtube.com/channel/UCkKGZJ-oZ6PhxFVI_Ppm26g"
-          // target="_blank"
-          // class="fl-icon fl-icon-color-branded fl-icon-youtube fl-icon-youtube-circle"
-        >Youtube</a>
-        <a
-          href="https://www.instagram.com/smiski_usa/"
-          // target="_blank"
-          // class="fl-icon fl-icon-color-branded fl-icon-instagram fl-icon-instagram-circle"
-        >Instagram</a>
+      <div className="col-md-6 col-sm-6 text-right clearfix">
+        <div className="icon-social-footer">
+          <a
+            href="https://www.facebook.com/smiski.japan/"
+            // target="_blank"
+            class="fl-icon-color-branded fl-icon-facebook fl-icon-facebook-circle"
+          >
+            Facebook
+          </a>
+          {/* <a
+            href="https://twitter.com/smiski_usa_"
+            // target="_blank"
+            // class="fl-icon fl-icon-color-branded fl-icon-twitter fl-icon-twitter-circle"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCkKGZJ-oZ6PhxFVI_Ppm26g"
+            // target="_blank"
+            // class="fl-icon fl-icon-color-branded fl-icon-youtube fl-icon-youtube-circle"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.instagram.com/smiski_usa/"
+            // target="_blank"
+            // class="fl-icon fl-icon-color-branded fl-icon-instagram fl-icon-instagram-circle"
+          >
+            Instagram
+          </a> */}
+        </div>
       </div>
     </footer>
   );
